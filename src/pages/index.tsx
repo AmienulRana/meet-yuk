32f1ea111401181c3dba3e85c5b6cc5c17a6ab49
unknown <amienulrana@gmail.com>
2024-01-10 04:56:01 +0700

    create layout for home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,38 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { useSocket } from '@/context/SocketContext'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import usePeer from '@/hooks/usePeer';
+import { useRouter } from 'next/router';
+import { v4 as uuidv4 } from 'uuid';
+import styles from '@/styles/home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const socket = useSocket();
-  usePeer();
+  const router = useRouter()
+  const [roomId, setRoomId] = useState('')
 
-  useEffect(() => {
-    socket?.on('connect', () => {
-      // console.log(socket?.id)
-    })
-  }, [socket])
-  return (  
-      <h1>Welcome</h1>
+  const createAndJoin = () => {
+    const roomId = uuidv4()
+    router.push(`/${roomId}`)
+  }
+
+  const joinRoom = () => {
+    if (roomId) router.push(`/${roomId}`)
+    else {
+      alert("Please provide a valid room id")
+    }
+  }
+  return (
+    <div className={'w-4/12 mx-auto p-2 border border-white rounded mt-8 text-white flex flex-col items-center'}>
+        <h1 className='text-xl text-center'>Google Meet Clone</h1>
+        <div className='flex flex-col items-center mt-3 w-full'>
+          <input className='text-black text-lg p-1 rounded w-9/12 mb-3' placeholder='Enter Room ID' value={roomId} onChange={(e) => setRoomId(e?.target?.value)}/>
+          <button className='bg-buttonPrimary py-2 px-4 rounded' onClick={joinRoom}>Join Room</button>
+        </div>
+        <span  className='my-3 text-xl' >--------------- OR ---------------</span>
+        <button className='bg-buttonPrimary py-2 px-4 rounded' onClick={createAndJoin}>Create a new room</button>
+    </div>
   )
 }
