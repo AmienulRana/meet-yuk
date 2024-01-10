8ea1d6de53924f12ef3d171d29cbf4e5eae10c57
unknown <amienulrana@gmail.com>
2024-01-11 02:10:07 +0700

    add the function for handle toggle video, audio and leave on meet

--- src/hooks/usePlayer.ts
+++ src/hooks/usePlayer.ts
@@ -3,12 +3,11 @@ import { cloneDeep } from 'lodash'
 import { useRouter } from 'next/router'
 import { useSocket } from '@/context/SocketContext'
 
-const usePlayer = (myId: string, roomId:string, peer:any) => {
+const usePlayer = (myId: string, roomId: string, peer:any) => {
     const socket = useSocket()
-    const [players, setPlayers] = useState({})
+    const [players, setPlayers] = useState<any>({})
     const router = useRouter()
     const playersCopy = cloneDeep(players)
-
     const playerHighlighted = playersCopy[myId]
     delete playersCopy[myId]
 
@@ -21,18 +20,18 @@ const usePlayer = (myId: string, roomId:string, peer:any) => {
     }
 
     const toggleAudio = () => {
-        setPlayers((prev) => {
+        setPlayers((prev: any) => {
             const copy = cloneDeep(prev)
-            copy[myId].muted = !copy[myId].muted
+            copy[myId].muted = !copy?.[myId]?.muted
             return {...copy}
         })
         socket?.emit('user-toggle-audio', myId, roomId)
     }
 
     const toggleVideo = () => {
-        setPlayers((prev) => {
+        setPlayers((prev: any) => {
             const copy = cloneDeep(prev)
-            copy[myId].playing = !copy[myId].playing
+            copy[myId].playing = !copy?.[myId]?.playing
             return {...copy}
         })
         socket?.emit('user-toggle-video', myId, roomId)
