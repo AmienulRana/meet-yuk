1be845ecbb2829afa316336a90e86dd6859cf9fa
unknown <amienulrana@gmail.com>
2024-01-11 23:20:44 +0700

    change styling for copy section component

--- src/components/CopySection.tsx
+++ src/components/CopySection.tsx
@@ -1,23 +1,20 @@
 import { CopyToClipboard } from "react-copy-to-clipboard";
-import { Copy } from "lucide-react";
+import { Link2 } from "lucide-react";
 
 import styles from "@/component/CopySection/index.module.css";
 import { ICopySection } from "@/libs/interface";
+import { IoLink } from "react-icons/io5";
 
 const CopySection = (props: ICopySection) => {
   const { roomId } = props;
 
   return (
-    <div className="flex flex-col absolute text-white border border-white rounded p-2 left-[30px] bottom-[100px]">
-      <div className="text-base">Copy Room ID:</div>
-      <hr className="my-1" />
-      <div className=" flex items-center text-sm">
-        <span>{roomId}</span>
-        <CopyToClipboard text={roomId as string}>
-          <Copy className="ml-3 cursor-pointer" />
-        </CopyToClipboard>
+    <CopyToClipboard text={roomId as string}>
+      <div className="flex md:max-w-[200px] w-max text-primary items-center gap-4 bg-lightblue rounded-full justify-center cursor-pointer p-2 px-3">
+        <IoLink className="min-w-max min-h-max text-lg" />
+        <span className="text-sm border-l-2 border-primary pl-3 truncate">1231-31321-3132</span>
       </div>
-    </div>
+    </CopyToClipboard>
   );
 };
 
