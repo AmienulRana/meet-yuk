ec401c000a7f9788128bab31530aa1533349036e
unknown <amienulrana@gmail.com>
2024-01-11 16:34:25 +0700

    change color in title logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,7 @@ export default function Logo(){
     return (
         <div className="flex justify-center items-center w-max flex-col">
           <Image src={'/logo.svg'} alt="logo" width={50} height={50} />
-          <p className="text-xl font-semibold -translate-y-2 font-mono">CallYuk</p>
+          <p className="text-xl text-primary font-semibold -translate-y-2 font-mono">CallYuk</p>
         </div>
     )
 }
