6669381f4b46b0144a0c499af85b767c9e70572b
unknown <amienulrana@gmail.com>
2024-01-13 12:42:43 +0700

    create backgorund blue if user on mic and red if user of mic

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -7,30 +7,44 @@ import { IPlayer } from "@/libs/interface";
 
 const Player = (props: IPlayer) => {
   const { url, muted, playing, isActive } = props;
+
+  const config = {
+    file: {
+      attributes: {
+        style: {
+          objectFit: 'cover',
+          height: '100%',
+          width: '100%',
+        },
+      },
+    },
+  };
   return (
     <div
       className={cx(
-        "relative overflow-hidden mb-5 h-[60vh] w-full bg-lightblue",
+        "relative overflow-hidden min-w-max  mb-5 bg-lightblue",
         {
-          "rounded-md h-min shadow-md w-[200px]": !isActive,
-          "rounded-lg": isActive,
+          "rounded-md h-[140px] w-[150px] shadow-md": !isActive,
+          "rounded-lg h-[60vh]": isActive,
           "flex items-center justify-center": !playing,
         }
       )}
     >
       <ReactPlayer
         url={url}
+        volume={muted ? 0 : 1}
         muted={muted}
         playing={true}
-        width="100%"
-        height="100%"
-        style={{ display: playing ? "block" : "none" }}
+        height={!isActive ? 140 : '100%'}
+        width={!isActive ? 150 : '100%'}
+        style={{ display: playing ? "block" : "none", objectFit: 'cover', transform: "scaleX(-1)" }}
+        config={config}
       />
       {!playing && (
         <UserSquare2 className={"text-white"} size={isActive ? 400 : 150} />
       )}
-      <div className="flex justify-between items-center absolute bottom-5 left-5 w-full">
-        <p className="bg-[rgba(0,0,0,.3)] px-2 py-1 text-xs text-white rounded-full">
+      <div className={`flex justify-between items-center absolute w-full ${isActive ? 'bottom-5 left-5 max-w-[250px]' : 'bottom-2 left-2 max-w-[100px]'}`}>
+        <p className="bg-[rgba(0,0,0,.3)] px-2 py-1 truncate text-xs text-white rounded-full">
           Joshep Adam
         </p>
       </div>
@@ -38,11 +52,11 @@ const Player = (props: IPlayer) => {
       {!isActive ? (
         muted ? (
           <MicOff
-            className={"text-white absolute right-2 bottom-2"}
-            size={20}
+            className={"text-white absolute rounded-full   right-2 bottom-2 p-1 bg-buttonPrimary"}
+            size={24}
           />
         ) : (
-          <Mic className={"text-white absolute right-2 bottom-2"} size={20} />
+          <Mic className={"text-white absolute right-2 rounded-full bottom-2 p-1 bg-primary"} size={24} />
         )
       ) : undefined}
     </div>
