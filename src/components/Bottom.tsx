07e4f4daff399038935891700b82e1120d3980ef
unknown <amienulrana@gmail.com>
2024-01-13 20:44:58 +0700

    Add important in class bg-primary in icon camera

--- src/components/Bottom.tsx
+++ src/components/Bottom.tsx
@@ -1,5 +1,13 @@
 import cx from "classnames";
-import { Mic, Video, PhoneOff, MicOff, VideoOff, Phone } from "lucide-react";
+import {
+  Mic,
+  Video,
+  PhoneOff,
+  MicOff,
+  VideoOff,
+  Phone,
+  Share,
+} from "lucide-react";
 
 import { IBottom } from "@/libs/interface";
 
@@ -10,7 +18,7 @@ const Bottom = (props: IBottom) => {
   return (
     <div
       className={
-        "absolute flex justify-between bottom-5 left-0 right-0 mx-auto w-[300px]"
+        "fixed w-9/12 flex justify-center items-center gap-10 bg-white border-r border-lightgray py-3 bottom-0 left-0 right-0"
       }
     >
       {muted ? (
@@ -20,10 +28,18 @@ const Bottom = (props: IBottom) => {
           onClick={toggleAudio}
         />
       ) : (
-        <Mic className={iconClass} size={55} onClick={toggleAudio} />
+        <Mic
+          className={cx(iconClass, "!bg-primary")}
+          size={55}
+          onClick={toggleAudio}
+        />
       )}
       {playing ? (
-        <Video className={iconClass} size={55} onClick={toggleVideo} />
+        <Video
+          className={cx(iconClass, "!bg-primary")}
+          size={55}
+          onClick={toggleVideo}
+        />
       ) : (
         <VideoOff
           className={cx(iconClass, "!bg-buttonPrimary")}
@@ -31,8 +47,16 @@ const Bottom = (props: IBottom) => {
           onClick={toggleVideo}
         />
       )}
-      <div title="Leave Meeting">
-      <Phone size={55} className={cx(iconClass, "active:!bg-buttonPrimary hover:bg-buttonPrimary")} onClick={leaveRoom} />
+      <div title="Share screen">
+        <Share
+          size={55}
+          className={cx(iconClass, "!bg-lightblue !text-primary")}
+        />
+      </div>
+      <div onClick={leaveRoom} title="Leave Meeting" className="cursor-pointer absolute top-1/2 -translate-y-1/2 right-5">
+        <p className="text-white bg-buttonPrimary rounded-full flex items-center justify-center text-sm px-4 py-1.5">
+          Leave Meet
+        </p>
       </div>
     </div>
   );
