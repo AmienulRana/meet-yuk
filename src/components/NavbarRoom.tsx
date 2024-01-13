c94a32f5b64a05fce716f9292e8b9932f83e6d55
unknown <amienulrana@gmail.com>
2024-01-13 20:44:28 +0700

    create border in navbar

--- src/components/NavbarRoom.tsx
+++ src/components/NavbarRoom.tsx
@@ -1,23 +1,23 @@
 import CopySection from "./CopySection";
 import Logo from "./Logo";
 
-export default function NavbarRoom(){
-    return (
-        <div className="flex md:px-10 px-4 items-center">
-        <div className="border-r pr-10 py-2 border-gray-200">
-          <Logo />
-        </div>
-        <div className="flex items-center flex-1 justify-between px-9">
-          <div className="">
-            <h1 className="text-xl font-semibold">
-              [Internal] Weekly Report Marketing + Sales
-            </h1>
-            <p className="text-sm mt-1 text-graytext">
-              Jan 12th, 2024 | 11:00 AM
-            </p>
-          </div>
-          <CopySection roomId="12345" />
-        </div>
+export default function NavbarRoom() {
+  return (
+    <div className="flex border-b-2 border-lightgray md:px-10 px-4 items-center">
+      <div className="border-r pr-10 py-2 border-gray-200">
+        <Logo />
       </div>
-    )
+      <div className="flex items-center flex-1 justify-between px-9">
+        <div className="">
+          <h1 className="text-xl font-semibold">
+            [Internal] Weekly Report Marketing + Sales
+          </h1>
+          <p className="text-sm mt-1 text-graytext">
+            Jan 12th, 2024 | 11:00 AM
+          </p>
+        </div>
+        <CopySection roomId="12345" />
+      </div>
+    </div>
+  );
 }
