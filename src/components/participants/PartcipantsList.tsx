a8cbbe4353d7a6a1b579854e03ec8e58f7bfd757
unknown <amienulrana@gmail.com>
2024-01-15 11:12:28 +0700

    render username in participant list

--- src/components/participants/PartcipantsList.tsx
+++ src/components/participants/PartcipantsList.tsx
@@ -1,14 +1,13 @@
 import { IPlayer } from "@/libs/interface";
 import StreamStatus from "../StreamStatus";
+import Avatar from "../Avatar";
 
-export default function ParticipantsList({ muted, playing }: IPlayer){
+export default function ParticipantsList({ muted, playing, username }: IPlayer){
     return (
         <div className="flex items-center bg-white px-5 my-[15px] py-2 rounded-full justify-between">
         <div className="flex items-center gap-3">
-          <div className="flex items-center bg-primary justify-center w-10 h-10 rounded-full">
-              <p className="text-white">A</p>
-          </div>
-          <span className="font-semibold text-base">Amienul Rana</span>
+          <Avatar text={username || 'A'} />
+          <span className="font-semibold text-base">{username}</span>
         </div>
           <StreamStatus muted={muted} playing={playing} />
       </div>
