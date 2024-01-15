e6179daa8133d6b603b20462415e3791f04b95f5
unknown <amienulrana@gmail.com>
2024-01-15 11:16:01 +0700

    send username to list participant

--- src/components/participants/Participants.tsx
+++ src/components/participants/Participants.tsx
@@ -7,17 +7,9 @@ export default function Participants({ players }: { players: any }) {
       <h2 className="text-lg font-semibold bg-white px-5 py-3">Participants {players &&  `(${Object.keys(players)?.length})`}</h2>
       <div className="px-4 custom-scrollbar overflow-y-auto h-[235px]">
         {Object.keys(players).map((playerId) => {
-          const { muted, playing } = players[playerId];
+          const { muted, playing, username } = players[playerId];
           return (
-            <Fragment key={playerId}>
-                <ParticipantsList muted={muted} playing={playing} key={playerId} />
-                <ParticipantsList muted={muted} playing={playing} key={playerId} />
-                <ParticipantsList muted={muted} playing={playing} key={playerId} />
-                <ParticipantsList muted={muted} playing={playing} key={playerId} />
-                <ParticipantsList muted={muted} playing={playing} key={playerId} />
-                <ParticipantsList muted={muted} playing={playing} key={playerId} />
-                <ParticipantsList muted={muted} playing={playing} key={playerId} />
-            </Fragment>
+                <ParticipantsList muted={muted} playing={playing} key={playerId} username={username} />               
           );
         })}
       </div>
