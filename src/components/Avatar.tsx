9d64a2c2b8f29fc96f0f43f7eb5419e9a8aa1578
unknown <amienulrana@gmail.com>
2024-01-16 09:37:59 +0700

    craete random background

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,9 +1,10 @@
 import { IAvatar } from "@/libs/interface";
+import { getRandomBrightColor } from "@/libs/utils";
 import cx from 'classnames';
 
 export default function Avatar({text, className}:IAvatar){
     return (
-        <div className={cx("flex items-center bg-primary justify-center min-w-[40px] min-h-[40px] rounded-full", className)}>
+        <div className={cx("flex items-center justify-center min-w-[40px] min-h-[40px] rounded-full", className)} style={{background: getRandomBrightColor()}}>
         <p className="text-white">{text?.split('')?.[0]}</p>
     </div>
     )
