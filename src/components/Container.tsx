ec4a2da8eb234a76ad3b07d574796201de8aa671
unknown <amienulrana@gmail.com>
2024-01-23 01:37:03 +0700

    add mx-auto class in container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -6,7 +6,7 @@ interface IContainer {
 
 export default function Container({ children, className}: IContainer){
     return (
-        <div className={`max-w-[1900px] md:px-16 sm:px-8 px-4 ${className}`}>
+        <div className={`max-w-[1900px] mx-auto md:px-16 sm:px-8 px-4 ${className}`}>
             {children}
         </div>
     )
