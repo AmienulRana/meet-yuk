5a0203f5645ad532293365e8fe5d9e55aa8fe237
unknown <amienulrana@gmail.com>
2024-01-23 06:02:48 +0700

    change the id to text

--- src/components/CopySection.tsx
+++ src/components/CopySection.tsx
@@ -12,7 +12,7 @@ const CopySection = (props: ICopySection) => {
     <CopyToClipboard text={roomId as string}>
       <div className="flex md:max-w-[200px] w-max text-primary items-center gap-4 bg-lightblue rounded-full justify-center cursor-pointer p-2 px-3">
         <IoLink className="min-w-max min-h-max text-lg" />
-        <span className="text-sm border-l-2 border-primary pl-3 truncate">1231-31321-3132</span>
+        <span className="text-sm border-l-2 border-primary pl-3 truncate">Copy Room ID</span>
       </div>
     </CopyToClipboard>
   );
