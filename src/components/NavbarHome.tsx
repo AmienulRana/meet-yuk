b240f354e1eeb1a796e22f20430504048439c1be
unknown <amienulrana@gmail.com>
2024-01-23 02:30:39 +0700

    open modal join room when button join is clicked

--- src/components/NavbarHome.tsx
+++ src/components/NavbarHome.tsx
@@ -1,13 +1,15 @@
+import useJoinModal from "@/hooks/useJoinRoomModal";
 import Button from "./Button";
 import Logo from "./Logo";
 
 export default function NavbarHome(){
+    const {onOpen} = useJoinModal();
     return (
         <nav className="bg-white">
             <div className="max-w-[1900px] flex justify-between mx-auto px-2 sm:px-6 lg:px-16">
                 <Logo />
                 <div className="flex items-center gap-4">
-                        <Button variant="outline" size="small" className="text-primary">
+                        <Button onClick={onOpen} variant="outline" size="small" className="text-primary">
                             Join Room
                         </Button>
                         <Button variant="background" size="small">
