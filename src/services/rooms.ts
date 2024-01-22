981fa597e45fe8e3bf4d807ec1f5870a5a84474b
unknown <amienulrana@gmail.com>
2024-01-23 06:02:16 +0700

    add service for call all api room

---
+++ src/services/rooms.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import toast from "react-hot-toast";
+
+export async function checkExistRoom(id: string) {
+  try {
+    const response  = await  axios.get(`/api/room?roomId=${id}`);
+    return response?.data;
+  } catch (error: any) {
+    if(error?.status === 500){
+        toast.error("Room id doesn't exist");    
+    }else{
+        toast.error('Failed to join Room');    
+    }
+    return false;
+  }
+}
+
+export async function getDetailRoomService(id: string) {
+  try {
+    const response  = await  axios.put(`/api/room?id=${id}`);
+    return response?.data;
+  } catch (error: any) {
+    console.log(error);
+  }
+}
