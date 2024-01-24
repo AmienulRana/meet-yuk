a684b3efb00b8b97c0364bf70b6be18b40578dd3
unknown <amienulrana@gmail.com>
2024-01-25 02:41:38 +0700

    render provider of tanstack

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,21 @@
-import { SocketProvider } from '@/context/SocketContext'
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { SocketProvider } from "@/context/SocketContext";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SocketProvider>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </SocketProvider>
-  )
+  );
 }
