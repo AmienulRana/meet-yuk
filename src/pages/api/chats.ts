e2d0902a24413f069d9481663edb83b6a0a8e0b0
unknown <amienulrana@gmail.com>
2024-01-25 02:39:20 +0700

    create api to handle request for chat collectiony

---
+++ src/pages/api/chats.ts
@@ -0,0 +1,36 @@
+import connectMongo from "@/libs/connectDb";
+import { PrismaClient } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+import Chats from "@/models/chats";
+
+export default async function room(req: NextApiRequest, res: NextApiResponse) {
+  await connectMongo();
+  if (req.method === "POST") {
+    try {
+      const newChat = await new Chats({
+        // kontol: req.body?.myId,
+        // pepek: "asdsadasda",
+        ...req.body,
+        // message: 'sdasdsada'
+      });
+      newChat?.save().then((res: any) => console.log(res));
+      return res.status(200).json({ message: "Successfully send new message" });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: "Failed to send new room" });
+    }
+  } else if (req.method === "GET") {
+    try {
+      console.log(req?.query);
+      const getAllChats = await Chats.find();
+      console.log("get all chat", getAllChats);
+      return res
+        .status(200)
+        .json(getAllChats);
+    } catch (error: any) {
+      return res
+        .status(500)
+        .json({ message: "Failed to find room", error: error });
+    }
+  }
+}
