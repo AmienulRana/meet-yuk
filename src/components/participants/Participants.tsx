2f024cda9c71f31cf5ca17123cc9962890215fdb
unknown <amienulrana@gmail.com>
2024-01-26 01:11:39 +0700

    add responsive in mobile view

--- src/components/participants/Participants.tsx
+++ src/components/participants/Participants.tsx
@@ -1,13 +1,32 @@
 import { Fragment } from "react";
 import ParticipantsList from "./PartcipantsList";
+import { ChevronDown } from "lucide-react";
+import { create } from "zustand";
+
+
+
+type Store = {
+  showParticipant: boolean;
+  toggleParticipant: () => void
+}
+
+export const useShowParticipant = create<Store>()((set) => ({
+  showParticipant: true,
+  toggleParticipant: () => set((state) => ({ showParticipant: !state.showParticipant })),
+}))
 
 export default function Participants({ players }: { players: any }) {
+  const { toggleParticipant, showParticipant } = useShowParticipant();
   return (
     <div className="flex-1">
-      <h2 className="text-lg font-semibold bg-white px-5 py-3">
+
+      <div className="flex justify-between items-center bg-white px-5 py-3"> 
+      <h2 className="text-lg font-semibold ">
         Participants {players && `(${Object.keys(players)?.length})`}
       </h2>
-      <div className="px-4 custom-scrollbar overflow-y-auto h-[235px]">
+      <ChevronDown size={25} onClick={toggleParticipant} className={`duration-300 ${showParticipant ? 'rotate-0' : 'rotate-180'}`}/>
+      </div>
+      <div className={`px-4 custom-scrollbar duration-300 overflow-y-auto ${showParticipant ? 'h-[235px]' : 'h-0'}`}>
         {Object.keys(players).map((playerId) => {
           const { muted, playing, username, color } = players[playerId];
           return (
