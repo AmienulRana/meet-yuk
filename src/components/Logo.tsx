5e7d7d6f539c522d2b0ad74f05b3574cf5f9eb9b
unknown <amienulrana@gmail.com>
2024-01-26 01:10:36 +0700

    add responsive in logo component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Logo(){
     return (
         <div className="flex justify-center items-center w-max flex-col">
-          <Image src={'/logo.svg'} alt="logo" width={50} height={50} />
+          <Image src={'/logo.svg'} alt="logo" width={50} height={50} className="w-10 h-10 md:w-[50px] md:h-[50px]" />
           <p className="text-xl text-primary font-semibold -translate-y-2 font-mono">CallYuk</p>
         </div>
     )
