73adc953715b984188670b90a0014c37cbd754e5
unknown <amienulrana@gmail.com>
2024-01-26 01:10:53 +0700

    create responsive in mobile view

--- src/components/NavbarRoom.tsx
+++ src/components/NavbarRoom.tsx
@@ -4,44 +4,58 @@ import Logo from "./Logo";
 import { getDetailRoomService } from "@/services/rooms";
 import { useRouter } from "next/router";
 import { Clock } from "./pages/room";
+import { FormInput, GripHorizontal } from "lucide-react";
+import { create } from "zustand";
 
 type TDetailRoom = {
-  name:string;
-  time:string;
-}
+  name: string;
+  time: string;
+};
+
 
 export default function NavbarRoom() {
-  const router= useRouter();
+  const router = useRouter();
 
-  const [detailRoom, setDetailRoom] = useState<TDetailRoom>()
+  const [detailRoom, setDetailRoom] = useState<TDetailRoom>();
+
 
   useEffect(() => {
-    async function getDetailRoom(){
+    async function getDetailRoom() {
       const data = await getDetailRoomService(router?.query?.roomId as string);
-      console.log(data);
-      setDetailRoom({name: data?.room?.name, time: data?.room?.createdAt})
+      setDetailRoom({ name: data?.room?.name, time: data?.room?.createdAt });
     }
 
     getDetailRoom();
-  }, [router])
+  }, [router]);
 
   return (
-    <div className="flex border-b-2 border-lightgray md:px-10 px-4 items-center">
-      <div className="border-r pr-10 py-2 border-gray-200">
+    <div className="flex justify-between md:justify-normal flex-wrap border-b-2 border-lightgray md:px-10 px-4 items-center">
+      <div className="md:border-r md:pr-10 pr-5 py-2 border-gray-200">
         <Logo />
       </div>
-      <div className="flex items-center flex-1 justify-between px-9">
-        <div className="">
-          <h1 className="text-xl font-semibold">
-            {detailRoom?.name}
-          </h1>
-          <Clock meetingTime={detailRoom?.time || ''} />
-          {/* <p className="text-sm mt-1 text-graytext">
-            Jan 12th, 2024 | 11:00 AM
-          </p> */}
-        </div>
-        <CopySection roomId={router?.query?.roomId as string || ''} />
+      <div className="flex items-center md:flex-1 justify-between md:px-9">
+        <TitleRoom className="hidden md:block" detailRoom={detailRoom} />
+        <CopySection roomId={(router?.query?.roomId as string) || ""} />
       </div>
+        <TitleRoom
+          className="w-full pb-3 mt-1 md:hidden"
+          detailRoom={detailRoom}
+        />
+        
+    </div>
+  );
+}
+
+interface ITitleRoom {
+  detailRoom: TDetailRoom | undefined;
+  className?: string;
+}
+
+export function TitleRoom({ detailRoom, className }: ITitleRoom) {
+  return (
+    <div className={className}>
+      <h1 className="text-xl font-semibold">{detailRoom?.name}</h1>
+      <Clock meetingTime={detailRoom?.time || ""} />
     </div>
   );
 }
