155328f4a4805e82bcbcd20f9c68b075f103d483
unknown <amienulrana@gmail.com>
2024-01-29 00:14:41 +0700

    remove unused code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -455,57 +455,6 @@ export default function Home() {
       <ModalCreateRoom />
 
       <Toaster  />
-      
-      {/* <div
-        className={
-          "w-4/12 mx-auto p-2 border border-white rounded mt-8 text-white flex flex-col items-center"
-        }
-      >
-        <h1 className="text-xl text-center">Google Meet Clone</h1>
-        <div className="flex flex-col items-center mt-3 w-full">
-          <input
-            className="text-black text-lg p-1 rounded w-9/12 mb-3"
-            placeholder="Room Name"
-            value={roomName}
-            onChange={(e) => {
-              setRoomName(e?.target?.value);
-            }}
-          />
-          <input
-            className="text-black text-lg p-1 rounded w-9/12 mb-3"
-            placeholder="Enter username"
-            value={username}
-            onChange={(e) => {
-              setUsername(e?.target?.value);
-            }}
-          />
-          <input
-            className="text-black text-lg p-1 rounded w-9/12 mb-3"
-            placeholder="Enter Room ID"
-            value={roomId}
-            onChange={(e) => setRoomId(e?.target?.value)}
-          />
-          <button
-            className={`bg-buttonPrimary py-2 px-4 rounded ${
-              loading && "opacity-60"
-            }`}
-            disabled={loading}
-            onClick={joinRoom}
-          >
-            Join Room
-          </button>
-        </div>
-        <span className="my-3 text-xl">--------------- OR ---------------</span>
-        <button
-          disabled={loading}
-          className={`bg-buttonPrimary py-2 px-4 rounded ${
-            loading && "opacity-60"
-          }`}
-          onClick={createAndJoin}
-        >
-          Create a new room
-        </button>
-      </div> */}
     </div>
   );
 }
